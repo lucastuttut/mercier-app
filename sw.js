3af43e9f7c0bfd349b4f960a3620eae42b582682
lucastuttut <93844115+lucastuttut@users.noreply.github.com>
2026-03-20 17:12:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-const CACHE_NAME = 'mercier-v44';
-const ASSETS = [
+const CACHE_NAME = 'mercier-v73';
+const assets = [
   './',
   './index.html',
-  './manifest.json'
+  './style.css',
+  './script.js',
+  'https://cdn.tailwindcss.com'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
