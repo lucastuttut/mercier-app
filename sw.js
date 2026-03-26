3195e0f4771a8421a61fdb0a5305c12077b3f98e
lucastuttut <93844115+lucastuttut@users.noreply.github.com>
2026-03-26 16:15:09 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'mercier-v87';
-const assets = [
+const CACHE_NAME = 'mercier-v106-safe'; // Atualizei a versão do cache
+const assets =[
   './',
   './index.html',
   './style.css',
@@ -22,6 +22,20 @@ self.addEventListener('activate', e => {
   );
 });
 
+// Nova Estratégia de Cache: Stale-While-Revalidate
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      const fetchPromise = fetch(e.request).then(networkResponse => {
+        // Guarda a versão mais atualizada silenciosamente
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, networkResponse.clone());
+        });
+        return networkResponse;
+      }).catch(() => cachedResponse); // Se estiver sem internet, usa o cache
+      
+      // Retorna o cache IMEDIATAMENTE se existir, enquanto a rede atualiza no fundo
+      return cachedResponse || fetchPromise;
+    })
+  );
 });
